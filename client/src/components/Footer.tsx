41a492c47e940893dac14e547623ccbed7e0a59e
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-05-25 13:52:26 +0200

    Added a link to admin page in the header which toggles from shopping cart is nor admin

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,11 +7,6 @@ function Footer() {
     <div>
       <div style={footerStyle}>
         © Created by FrontFelix, AdaHep and MiMeiner, 2022.
-        {/* <Link to="/Admin" style={{textDecoration: 'none'}}>
-          <Button style={StyledButton} variant="contained" href="">
-            ADMIN
-          </Button>
-        </Link> */}
       </div>
     </div>
   );
