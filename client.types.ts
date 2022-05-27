549a207ceb850507823cc8830b500f23ec44e1d7
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-05-27 15:39:18 +0200

    created a context for adminorders Co-authored-by: elinarnten <elinarnten@users.noreply.github.com>

--- client.types.ts
+++ client.types.ts
@@ -3,7 +3,7 @@ import { Order, User } from "./server/resources";
 // Ta bort customer från order och lägg till den igen med typen User
 export type ClientOrder = Omit<Order, "customer"> & {
   _id: string;
-  customer: User;
+  customer?: User;
 };
 
 export type ClientUser = User & { _id: string };
---
+++ client/src/components/context/AdminOrderContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useEffect, useState, FC, useCallback } from "react";
+import { makeReq } from "../../helper";
+import UserProvider, { UserContext } from "./LoginContext";
+import { ObjectId } from 'mongoose';
+
+export interface Order {
+  customer: ObjectId;
+  products: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export type ClientOrder = Omit<Order, "customer"> & {
+  _id: string;
+  customer?: User;
+};
+
+export interface User {
+  email: string;
+  isAdmin: boolean;
+}
+
+interface AdminOrderContext {
+    orders: Order[];
+    getOrders: () => void;
+}
+
+
+export const AdminOrderContext = createContext<AdminOrderContext>({
+    orders: [],
+    getOrders: () => {},
+});
+
+const AdminOrderProvider: FC = (props: any) => {
+    const [orders, setOrders] = useState<Order[]>([]);
+    /* add state for quantity */
+    
+  const getOrders = useCallback(async () => {
+      let order = await makeReq("/api/orders", "GET");
+        setOrders(order);
+  }, []);
+
+  return (
+      <AdminOrderContext.Provider
+          value={{
+            orders,
+            getOrders,
+          }}
+      >
+          {props.children}
+      </AdminOrderContext.Provider>
+  );
+}
+
+export const useAdminOrder = () => useContext(AdminOrderContext)
+
+export default AdminOrderProvider;
