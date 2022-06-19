13dd455216be127bf2547100c165256b4211ca1f
AdaHep <adam.hepsever@gmail.com>
2022-06-19 20:01:49 +0200

    Fix

--- server/resources/order/order.model.ts
+++ server/resources/order/order.model.ts
@@ -24,7 +24,7 @@ const OrderSchema = new mongoose.Schema<Order>(
     customer: { type: Schema.Types.ObjectId, ref: "user", required: false },
     products: { type: [ProductSchema], required: true },
     deliveryAddress: { type: AddressSchema, required: true },
-    deliveryOption: { type: deliveryOptionSchema, required: true },
+    deliveryOption: { type: deliveryOptionSchema, required: false },
     paymentMethod: { type: String, required: true },
     isSent: { type: Boolean, required: true, default: false },
     createdAt: { type: Date, required: true, default: Date.now },
